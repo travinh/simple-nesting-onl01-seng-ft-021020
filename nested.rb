6830b3eb566ee200ff79db9144c429a43d1d0f72
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 08:09:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,9 @@ def adding_matz
         }
      }
     
-  programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
-  programmer_hash[:yukihiro_matsumoto][:languages] << "C"
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
+  programmer_hash[:yukihiro_matsumoto][:languages] = "C"
   
 
     
