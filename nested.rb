ca9f2af0d2304981b17f583fef9eaf166ef676b9
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 07:50:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+  
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
@@ -129,5 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages] <<"Assembly"
 
 end
