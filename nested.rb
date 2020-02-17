f826ea6a970d0b1a30c399eb62e3ab08090b9171
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 08:52:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,5 @@ def adding_to_dennis
      }
   
   programmer_hash[:dennis_ritchie][:languages]<<  "Assembly"
-  programmer_hash[:dennis_ritchie][:languages]
-
+  programmer_hash[:dennis_ritchie]
 end
